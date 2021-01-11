03993272d9d3bc837534c9906d0e78d1a07c3d70
guysh95 <shnaiderguy@gmail.com>
2021-01-11 21:48:30 +0200

    Fixed css styling, added usage of django extends feature. added connectivity via navbar.

--- SRC/APPLICATION-SOURCE-CODE/el_critico/finder/urls.py
+++ SRC/APPLICATION-SOURCE-CODE/el_critico/finder/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='finder_home'),
-    path('critic=<str:critic>/', views.ReviewsByCritic.as_view()),
+    path('critic=<str:critic>/', views.ReviewsByCritic.as_view()),  # search by critic name
     path('popular/', views.PopularMoviesUsers.as_view(), name='finder_popular'),
     path('critic-pick/', views.PopularMoviesCritics.as_view(), name='finder_critics'),
-    path('phrase=<str:phrase>/', views.KeyWordReview.as_view()),
-    path('critic-pick/filter/<str:critic>', views.MoviesPicked.as_view()),
-    path('critic-pick/filter/genre/<str:genre>', views.MoviesPickedGenre.as_view()),
-    path('stats/users', views.GenreUsersStats.as_view())
+    path('phrase=<str:phrase>/', views.KeyWordReview.as_view()),    # search by given phrase (context)
+    path('critic-pick/filter/<str:critic>', views.MoviesPicked.as_view()),  # search stats by critic name
+    path('critic-pick/filter/genre/<str:genre>', views.MoviesPickedGenre.as_view()),    # search stats by genere
+    path('stats/users', views.GenreUsersStats.as_view(), name='finder_users')
 ]
