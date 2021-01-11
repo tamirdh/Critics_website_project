1769495b3a7875ba4ad8c99930e5e4cb33cf3aed
guysh95 <shnaiderguy@gmail.com>
2021-01-11 12:56:59 +0200

    updating index.html, adding name to movies_by_critics_picks

--- SRC/APPLICATION-SOURCE-CODE/el_critico/finder/urls.py
+++ SRC/APPLICATION-SOURCE-CODE/el_critico/finder/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='finder_home'),
     path('critic=<str:critic>/', views.ReviewsByCritic.as_view()),
     path('popular/', views.PopularMoviesUsers.as_view(), name='finder_popular'),
-    path('critic-pick/', views.PopularMoviesCritics.as_view()),
+    path('critic-pick/', views.PopularMoviesCritics.as_view(), name='finder_critics'),
     path('phrase=<str:phrase>/', views.KeyWordReview.as_view()),
     path('critic-pick/filter/<str:critic>', views.MoviesPicked.as_view()),
     path('critic-pick/filter/genre/<str:genre>', views.MoviesPickedGenre.as_view()),
